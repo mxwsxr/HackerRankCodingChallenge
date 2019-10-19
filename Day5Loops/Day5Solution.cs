21bab123c36305eb81fc3bc9e0694294a365c2b0
Albert <Albert@DESKTOP-923AJ44>
2019-10-19 18:49:30 +0200

    added Day 5 Solution

--- Day5Loops/Day5Solution.cs
+++ Day5Loops/Day5Solution.cs
@@ -6,7 +6,13 @@ namespace Day5Loops
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int result = n * i;
+                Console.WriteLine(n + " x " + i + " = " + result);
+            }
         }
     }
 }
