1e168a1677723e75be365f479b253892f707eaba
Torben Clasen <torbenclasen@gmail.com>
2019-10-27 02:08:02 +0200

    Modifications in the name of testability

---
+++ Day8DictionariesMaps/CommInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day8DictionariesMaps
+{
+    public interface ICommInterface
+    {
+        string ReadLine();
+
+        void WriteLine(string value);
+    }
+}
---
+++ Day8DictionariesMaps/ConsoleImpl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day8DictionariesMaps
+{
+    public class ConsoleImpl : ICommInterface
+    {
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+
+        public void WriteLine(string value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
