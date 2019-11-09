a1f94cf0cceea263775174fee66c4f35721fd6c0
Albert <Albert@DESKTOP-923AJ44>
2019-11-09 01:46:44 +0100

    readded day9solution

---
+++ Day9Recursion/Day9Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Day9Recursion
+{
+    public class Day9Solution
+    {
+        private static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int result = Factorial(n);
+
+            Console.WriteLine(result);
+        }
+
+        private static int Factorial(int n)
+        {
+            return n == 1 ? 1 : n * Factorial(n - 1);
+        }
+    }
+}
