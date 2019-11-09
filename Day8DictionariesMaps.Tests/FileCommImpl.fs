d1c7f6dff6b4ffbc9a1d1d7822ed28db8836da05
Albert <t_che95@hotmail.de>
2019-11-09 01:35:59 +0100

    Revert "Coding progress"

--- Day8DictionariesMaps.Tests/FileCommImpl.fs
+++ Day8DictionariesMaps.Tests/FileCommImpl.fs
@@ -7,21 +7,19 @@ type FileImpl(input,results) =
     member this.Input : string list = input
     do if File.Exists results then
             do File.Delete results
-
+    
     let result = new StreamWriter(File.Create(results))
 
     member this.Results = result
+        
 
     member this.Close () = this.Results.Close()
 
     interface Day8DictionariesMaps.ICommInterface with
-        member this.ReadLine(): string =
-            if this.index < this.Input.Length then
-                let res = this.Input.[this.index]
-                this.index <- this.index + 1
-                res
-            else
-                ""
+        member this.ReadLine(): string = 
+            let res = this.Input.[this.index]
+            this.index <- this.index + 1
+            res
 
-        member this.WriteLine(value: string): unit =
+        member this.WriteLine(value: string): unit = 
             this.Results.WriteLine(value)
