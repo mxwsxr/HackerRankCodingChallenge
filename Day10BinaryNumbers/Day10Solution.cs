9cae45512ef5eddbc4d5fb95ecbb2729ea7e8254
Albert <Albert@DESKTOP-923AJ44>
2019-11-10 14:08:47 +0100

    added Day10Solution

---
+++ Day10BinaryNumbers/Day10Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Day10BinaryNumbers
+{
+    internal class Day10Solution
+    {
+        /// <summary>
+        /// https://davidzych.com/converting-an-int-to-a-binary-string-in-c/
+        /// We use Convert.ToString(n, 2) in here to take input n
+        /// and convert it to binary base-2.
+        /// Furthermore, Split takes the string and removes the 0s.
+        /// .Max gives the max of the consecutive 1s
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(
+                Convert.ToString(
+                    Convert.ToInt32(Console.ReadLine()), 2)
+                .Split('0')
+                .Max(binary => binary.Length)
+                );
+        }
+    }
+}
